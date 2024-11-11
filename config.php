c5284768ba88a34422fa43bbb46cf516d6b22d9f
NEON <mgutie33@hotmail.com>
2024-11-10 20:56:35 -0500

    first commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$db = [
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'db' => 'crudrapido' //Cambiar al nombre de tu base de datos
+];
+
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Sample PHP code for youtube.videos.update
+ * See instructions for running these code samples locally:
+ * https://developers.google.com/explorer-help/code-samples#php
+ */
+
+header("HTTP/1.1 200");
+
+?>
+
+
+<!--
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Visualizer</title>
+</head>
+<body>
+
+</body>
+</html>
+-->
